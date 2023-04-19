7b2984c7d88020894e2cbdc9e0e1a5feeb8de084
edoardosartori <100801137+edoardosartori@users.noreply.github.com>
2023-04-19 20:26:32 +0200

    seems to work fine
    TODO:
    - add android "distro"
    - fully test it

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,53 +1,84 @@
 <template>
   <ion-page>
+    <ion-header :translucent="true" collapse="onMobile">
+      <ion-toolbar>
+        <ion-title> Calculator </ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-content :fulldisplay="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Calculator</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
       <div class="container">
-        <div class="display">-</div>
-        <div class="display main-display">0</div>
-        <div>
-          <div class="buttons-row">
-            <button class="button delete-btn">C</button>
-            <button class="button delete-btn">&larr;</button>
-            <button class="button operator-btn">%</button>
-            <button class="button operator-btn">&divide;</button>
-          </div>
-          
-          <div class="buttons-row">
-            <button class="button">7</button>
-            <button class="button">8</button>
-            <button class="button">9</button>
-            <button class="button operator-btn">&times;</button>
-          </div>
-          
-          <div class="buttons-row">
-            <button class="button">4</button>
-            <button class="button">5</button>
-            <button class="button">6</button>
-            <button class="button operator-btn">&minus;</button>
-          </div>
-          
-          <div class="buttons-row">
-            <button class="button">1</button>
-            <button class="button">2</button>
-            <button class="button">3</button>
-            <button class="button operator-btn">&plus;</button>
-          </div>
-          
-          <div class="buttons-row">
-            <button class="button double">0</button>
-            <button class="button">.</button>
-            <button class="button operator-btn">&equals;</button>
-          </div>
+        <div class="display">{{ calcs || '-' }}</div>
+        <div class="display main-display">{{ buffer || 0 }}</div>
+      
+      <div>
+        <div class="buttons-row">
+          <button class="button delete-btn double"
+            v-on:click="this.reset()">
+            C
+          </button>
+          <button class="button delete-btn"
+            v-on:click="this.delete()">
+            &larr;
+          </button>
+          <button class="button operator-btn"
+            v-on:click="this.handleSymbol('/')">
+            &divide;
+          </button>
+        </div>
+        
+        <div class="buttons-row">
+          <button class="button"
+            v-on:click="this.handleNumber('7')">7</button>
+          <button class="button"
+            v-on:click="this.handleNumber('8')">8</button>
+          <button class="button"
+            v-on:click="this.handleNumber('9')">9</button>
+          <button class="button operator-btn"
+            v-on:click="this.handleSymbol('*')">
+            &times;
+          </button>
+        </div>
+
+        <div class="buttons-row">
+          <button class="button"
+            v-on:click="this.handleNumber('4')">4</button>
+          <button class="button"
+            v-on:click="this.handleNumber('5')">5</button>
+          <button class="button"
+            v-on:click="this.handleNumber('6')">6</button>
+          <button class="button operator-btn"
+            v-on:click="this.handleSymbol('-')">
+            &minus;
+          </button>
+        </div>
+
+        <div class="buttons-row">
+          <button class="button"
+            v-on:click="this.handleNumber('1')">1</button>
+          <button class="button"
+            v-on:click="this.handleNumber('2')">2</button>
+          <button class="button"
+            v-on:click="this.handleNumber('3')">3</button>
+          <button class="button operator-btn"
+            v-on:click="this.handleSymbol('+')">
+            &plus;
+          </button>
+        </div>
+
+        <div class="buttons-row">
+          <button class="button double"
+            v-on:click="this.handleNumber('0')">0</button>
+          <button class="button"
+            v-on:click="this.handleComma()">.</button>
+          <button class="button operator-btn"
+          v-on:click="this.getResult()">
+            &equals;
+          </button>
         </div>
       </div>
-    </ion-content>
-  </ion-page>
+    </div>
+  </ion-content>
+</ion-page>
 </template>
 
 <script src="../script/homepage.js"/>
